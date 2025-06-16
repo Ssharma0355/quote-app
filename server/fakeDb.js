592fdce7b837eb58fc0f0963bd2a11adacf76765
sachin_sde <ssachin.s@myclnq.co.in>
2025-06-16 16:19:14 +0530

    set-data-fetching-by-fakedb

---
+++ server/fakeDb.js
@@ -0,0 +1,25 @@
+export const users = [
+    {
+        id:123,
+        firstName:"Sachin",
+        lastName:"Sharma",
+        email:"ssharma0355@gmail.com"
+    },
+    {
+        id:456,
+        firstName:"Detective",
+        lastName:"Manglu",
+        email:"detective@manglu.com"
+    }
+]
+
+export const quotes = [
+  {
+    name: "I do hardCore codes",
+    by: "123",
+  },
+  {
+    name:"20 rupye doge to kaam ki baat bataunga",
+    by:"456"
+  }
+];
--- server/server.js
+++ server/server.js
@@ -2,22 +2,34 @@ import {ApolloServer,gql} from "apollo-server";
 
 import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
 
+import { quotes,users } from "./fakeDb.js";
+
 //creating schema 
 const typeDefs = gql`
-  type Query{
-    greet:String
+  type Query {
+    users: [User]
+    quotes: [Quote]
   }
-`
+  type User {
+    id: ID
+    firstName: String
+    lastName: String
+    email: String
+  }
+  type Quote {
+    name:String
+    by:ID
+  }
+`;
 
 // resolver
 
 const resolvers = {
-Query:{
-    greet:()=>{
-       return "Hello Sachin"
-    }
-}
-}
+  Query: {
+    users: () => users,
+    quotes: () => quotes,
+  },
+};
 
 const server = new ApolloServer({
   typeDefs,
