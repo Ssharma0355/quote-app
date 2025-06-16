6871507418e49bb1362c5902cfeeaf1bdefa0cf3
sachin_sde <ssachin.s@myclnq.co.in>
2025-06-16 16:02:45 +0530

    graphQL-server-is-set-with-playground

--- server/server.js
+++ server/server.js
@@ -1,2 +1,30 @@
 import {ApolloServer,gql} from "apollo-server";
+
 import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
+
+//creating schema 
+const typeDefs = gql`
+  type Query{
+    greet:String
+  }
+`
+
+// resolver
+
+const resolvers = {
+Query:{
+    greet:()=>{
+       return "Hello Sachin"
+    }
+}
+}
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
+});
+
+server.listen().then(({url})=>{
+    console.log(`Server sunning ${url}`)
+})
